79fc5e8fd1b08bdcc35127a8359d1fbfd960e490
ken <ken@192.168.1.104>
2018-08-25 11:07:29 +0800

    2018-08-25

---
+++ src/main/java/com/ken/MySpringBootApplication.java
@@ -0,0 +1,30 @@
+package com.ken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ch.qos.logback.core.db.DataSourceConnectionSource;
+
+@RestController
+@SpringBootApplication(exclude= {DataSourceConnectionSource.class})
+/**
+ * exclude关闭特定的自动配置
+ * @SpringBootApplication融合了以下三个注解(也可以直接使用下面三个注解)：
+ * @SpringBootConfiguration()
+ * @EnableAutoConfiguration(让SpringBoot根据路径中的jar包依赖，为档期项目进行自动配置)
+ * @ComponentScan()
+ *
+ */
+public class MySpringBootApplication {
+
+	@RequestMapping("/index")
+	public String index() {
+		return "Srping Boot!";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBootApplication.class, args);
+	}
+}
