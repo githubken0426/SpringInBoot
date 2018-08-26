63af5013f88c4290189eaf2389da0deaa83f2be6
ken <ken@192.168.1.105>
2018-08-26 08:53:58 +0800

    20180826

--- src/main/java/com/ken/MySpringBootApplication.java
+++ src/main/java/com/ken/MySpringBootApplication.java
@@ -2,13 +2,12 @@ package com.ken;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import ch.qos.logback.core.db.DataSourceConnectionSource;
-
 @RestController
-@SpringBootApplication(exclude= {DataSourceConnectionSource.class})
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 /**
  * exclude关闭特定的自动配置
  * @SpringBootApplication融合了以下三个注解(也可以直接使用下面三个注解)：
